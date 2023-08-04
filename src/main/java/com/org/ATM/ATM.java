fa43cbbac12cf46bad3c5abd7412653cdc26a530
Suvendu kumar Mohanty <mohantysuvendu5@gmail.com>
2023-08-04 23:10:43 +0530

    First Commit.

---
+++ src/main/java/com/org/ATM/ATM.java
@@ -0,0 +1,15 @@
+package com.org.ATM;
+
+import java.io.IOException;
+
+public class ATM {
+
+    public static void main(String[] args) throws IOException {
+        OptionMenu optionMenu = new OptionMenu();
+        introduction();
+        optionMenu.mainMenu(); 
+    }
+    public static void introduction(){
+        System.out.println("WELCOME TO THE ATM PROJECT! ");
+    }
+}
---
+++ src/main/java/com/org/ATM/App.java
@@ -0,0 +1,7 @@
+package com.org.ATM;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
